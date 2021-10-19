46b21f61acdb5dcc85783bb04f9e75f16b8e26e5
Denisse <denissep.orellana@gmail.com>
2021-10-19 00:33:41 -0300

    update

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     title();
     addImage('./assets/img/boostrap.png');
     addParagraph('Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.');
+    addCardColumn();
 })
 
 function addImage(image) {
@@ -31,11 +32,39 @@ function title() {
 }
 
 function addParagraph(text) {
-    var paragraph = document.createElement('p');
-    var textParagraph = document.createTextNode(text);
+    let paragraph = document.createElement('p');
+    let textParagraph = document.createTextNode(text);
     paragraph.className = 'text-center mt-2';
+    paragraph.id = 'pg'
     paragraph.appendChild(textParagraph);
     let title = document.getElementById('h2');
     title.insertAdjacentElement('afterend', paragraph);
 }
 
+function addCardColumn() {
+    let divRow = document.createElement('div');
+    divRow.className = 'row'
+    divRow.id = 'divRow'
+    let paragraph = document.getElementById('pg')
+    paragraph.insertAdjacentElement('afterend', divRow);
+    
+    let divCol = document.createElement('div');
+    divCol.className = 'col-md-4 order-md-2 mb-4';
+    divCol.id = 'divCol'
+    document.getElementById('divRow').appendChild(divCol);
+
+    let h4 = document.createElement('h4')
+    h4.className = 'd-flex justify-content-center align-items-center mb-3';
+    document.getElementById('divCol').appendChild(h4);
+
+    let spanOne = document.createElement("span");
+    spanOne.className = 'text-muted'; 
+    spanOne.innerHTML = 'Your cart &emsp;&emsp;';
+    // spanOne.appendChild(document.createTextNode('Your cart'));
+    h4.appendChild(spanOne);
+
+    let spanTwo = document.createElement("span");
+    spanTwo.className = 'badge badge-secondary badge-pill';
+    spanTwo.innerHTML = '3';
+    h4.appendChild(spanTwo);
+}
